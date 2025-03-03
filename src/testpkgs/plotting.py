c3f0aa96092538bbd8eccdb6d41df93d9db4dfa6
Lu Hou Yang <luhouyang@gmail.com>
2025-03-03 17:29:34 +0800

    plotting

---
+++ src/testpkgs/plotting.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+import matplotlib.pyplot as plt
+
+
+def plot_words(word_counts: Counter, n=10):
+    top_n_words = word_counts.most_common(n)
+    word, count = zip(*top_n_words)
+    fig = plt.bar(range(n), count)
+    plt.xticks(range(n), labels=word, rotation=45)
+    plt.xlabel("Word")
+    plt.ylabel("Count")
+    return fig
