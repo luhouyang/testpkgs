8dff927e82f196ac40f8f39c112b55b4e7e6ba30
Lu Hou Yang <luhouyang@gmail.com>
2025-03-04 01:00:03 +0800

    docs: comments

--- src/testpkgs/testpkgs.py
+++ src/testpkgs/testpkgs.py
@@ -3,6 +3,22 @@ from string import punctuation
 
 
 def load_text(file: str):
+    """Load text from a text file and return as a string.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    str
+        Text file contents.
+
+    Examples
+    --------
+    >>> load_text("text.txt")
+    """
     with open(file, "r") as f:
         text = f.read()
 
@@ -10,14 +26,49 @@ def load_text(file: str):
 
 
 def clean_text(text: str):
+    """Lowercase and remove punctuation from a string.
+
+    Parameters
+    ----------
+    text : str
+        Text to clean.
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    Examples
+    --------
+    >>> clean_text("Early optimization is the root of all evil!")
+    'early optimization is the root of all evil'
+    """
     text = text.lower()
     for p in punctuation:
         text = text.replace(p, "")
     return text
 
 
-def count_words(file: str):
-    text = load_text(file)
+def count_words(input_file: str):
+    """Count words in a text file.
+
+    Words are made lowercase and pucntuation is removed before counting.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file
+
+    Returns
+    -------
+    collections.Counter
+        dict-like object where keys are words and values are counts.
+
+    Examples
+    --------
+    >>> count_words("text.txt")
+    """
+    text = load_text(input_file)
     text = clean_text(text)
     words = text.split()
     return Counter(words)
